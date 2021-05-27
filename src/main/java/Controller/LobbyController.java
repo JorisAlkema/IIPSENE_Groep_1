38bdabf325442e2166b07945d727250fa4bb97f4
Stefan So <s1126025@student.hsleiden.nl>
2021-05-27 17:16:32 +0200

    Created Lobby System, Not finished yet

---
+++ src/main/java/Controller/LobbyController.java
@@ -0,0 +1,21 @@
+package Controller;
+
+import Model.Lobby;
+import Service.Observer;
+import javafx.stage.Stage;
+
+public class LobbyController {
+    private Lobby lobby;
+    public LobbyController(Stage primaryStage, String player_uuid, String roomCode) {
+        lobby = new Lobby(primaryStage, player_uuid, roomCode);
+    }
+
+    public void leaveRoom() {
+        lobby.disconnect();
+        lobby.returnToMenu();
+    }
+
+    public void addObserver(Observer observer) {
+        lobby.addObserver(observer);
+    }
+}
