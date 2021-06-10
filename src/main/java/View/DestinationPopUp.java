d679866ea03554238fe3341f8c8ca2287d55bb68
Jasper Keijzer <jasperkeijzer96@gmail.com>
2021-06-10 12:01:59 +0200

    DestTicketPopUp show properly on first turn
    The popup now waits for any previous players that also have their first turn to confirm their ticket choices, then ends their turn. You only get the popup when it's actually your turn now

--- src/main/java/View/DestinationPopUp.java
+++ src/main/java/View/DestinationPopUp.java
@@ -89,9 +89,9 @@ public class DestinationPopUp {
         Scene scene = new Scene(vBox);
         scene.getStylesheets().add(MainState.MenuCSS);
         stage.setScene(scene);
-        stage.showAndWait();
         stage.setAlwaysOnTop(true);
         stage.setX(WINDOW_X_POSITION);
         stage.setY(WINDOW_Y_POSITION);
+        stage.showAndWait();
     }
 }
