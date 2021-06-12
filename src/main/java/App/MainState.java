ac9c6bcf6d2484b03cbcc22a85f94b2fe9c274e1
Jasper Keijzer <jasperkeijzer96@gmail.com>
2021-06-12 10:03:38 +0200

    Added css to EndGameView when called from GameController

--- src/main/java/App/MainState.java
+++ src/main/java/App/MainState.java
@@ -1,6 +1,5 @@
 package App;
 
-import Model.MusicPlayer;
 import Model.Player;
 import Service.FirebaseService;
 import javafx.stage.Stage;
@@ -10,19 +9,13 @@ public class MainState {
     public static Stage primaryStage;
 
     public static String roomCode;
-
     public static String player_uuid;
 
-    public static MusicPlayer musicPlayer;
-
-
-    public static final int WINDOW_X_POSITION = 20;
-    public static final int WINDOW_Y_POSITION = 50;
     public static final int WINDOW_WIDTH = 1510;
     public static final int WINDOW_HEIGHT = 883;
 
-    public static final String menuCSS = "css/mainMenuStyle.css";
-    public static final String gameCSS = "css/mainStyle.css";
+    public static final String menuCSS = "css/mainMenu.css";
+    public static final String gameCSS = "css/gameView.css";
 
     public static Player getLocalPlayer() {
         return firebaseService.getPlayerFromLobby(roomCode, player_uuid);
